dae6ce9b5338d3fa39470b8ec744c26b669c87e5
Stephen Engler <sengler1989@gmail.com>
2018-07-06 20:43:47 -0500

    restructured the code to make more clear, added docstring

--- is_palindrome.py
+++ is_palindrome.py
@@ -1,11 +1,25 @@
 def is_palindrome(input):
+    """
+    If input is a palindrome, returns true
+    else returns false
+    input string
+    output boolean
+    """
+    #Removes any spaces
     input = input.replace(" ", "")
     length = len(input)
+    #iterates over the string
+    #if first and last letters don't match returns false
+    #if they do match increments the counter for first letter and decrements 
+    #the counter for the last letter and checks again
+    #if the counter for the first letter is greater than the last,
+    #it means all letters have been checked and match 
+    #returns true
     for x in range(length):
-        if input[x] != input[length -1]:
-            return False
         length -= 1
-    return True
-
-
+        if x >= length:
+            return True
+        elif input[x] != input[length]:
+            return False
+        
 print(is_palindrome('race car'))
