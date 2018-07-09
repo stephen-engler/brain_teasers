9b932521096f70dd5583d20872b30dcf0fb064db
Stephen Engler <sengler1989@gmail.com>
2018-07-09 15:06:56 -0500

    add docstring

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,11 @@
 def fizzbuzz():
+    """
+    for numbers 1 to 100, if number is divisible by 3 prints fizz
+    if divisible by 5 prints buzz, if by both, fizzbuzz
+    if none, prints the number
+    input: none
+    output: none
+    """
     for x in range(1,101):
         if x%3==0 and x%5==0:
             print('fizzbuzz')
